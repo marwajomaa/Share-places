82128c338b58fadde964c0d2aeafbb7c0f4ae446
marwa <marwajomaa4@gmail.com>
2020-09-17 13:27:51 +0300

    add auth context for app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -12,21 +12,33 @@ import UserPlaces from "./pages/places/UserPlaces";
 import NewPlace from "./pages/places/NewPlace";
 import UpdatePlace from "./pages/places/UpdatePlace";
 import Auth from "./pages/Users/Auth";
+import { AuthContext } from "./context/auth-context";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = setState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" component={Users} exact />
-          <Route path="/:userId/places" component={UserPlaces} exact />
-          <Route path="/places/new" component={NewPlace} exact />
-          <Route path="/places/:pId" component={UpdatePlace} exact />
-          <Route path="/auth" component={Auth} exact />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.provider value={(isLoggedIn, login, logout)}>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/" component={Users} exact />
+            <Route path="/:userId/places" component={UserPlaces} exact />
+            <Route path="/places/new" component={NewPlace} exact />
+            <Route path="/places/:pId" component={UpdatePlace} exact />
+            <Route path="/auth" component={Auth} exact />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.provider>
   );
 };
 
