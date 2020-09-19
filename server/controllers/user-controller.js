d0df17eef4264a3fefee5d364abc35807352a4f1
marwa <marwajomaa4@gmail.com>
2020-09-20 00:49:50 +0300

    setup user routes

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,77 @@
+const HttpError = require("./../models/http-error");
+
+let DUMMY_USERS = [
+  {
+    id: Math.random(),
+    name: "Marwa",
+    email: "marwa@marwa.com",
+    password: "12341234",
+  },
+];
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    res.status(200).json({ users: DUMMY_USERS });
+
+    if (!DUMMY_USERS) {
+      next(new HttpError("No users found", 404));
+    }
+  } catch (err) {
+    next(new HttpError("Something went wrong", 500));
+  }
+};
+exports.createUser = async (req, res, next) => {
+  const data = req.body;
+  const { name, email, password } = data;
+  const newUser = {
+    id: Math.random(),
+    name,
+    email,
+    password,
+  };
+
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (hasUser) {
+    return next(
+      new HttpError("Could not create user, email already exists", 422)
+    );
+  }
+
+  const user = await DUMMY_USERS.push(newUser);
+
+  res.status(200).json({
+    status: "success",
+    message: "user has been successfully created",
+    users: DUMMY_USERS,
+  });
+  try {
+  } catch (err) {
+    next(new HttpError("Something went wrong", 500));
+  }
+};
+
+exports.loginUser = async (req, res, next) => {
+  const data = req.body;
+  const { email, password } = data;
+  try {
+    const user = DUMMY_USERS.find((user) => user.email === email);
+
+    if (!user || user.password !== password) {
+      return next(
+        new HttpError(
+          "Could not identify user, credentials seem to be wrong",
+          404
+        )
+      );
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "User with the given email and password founded",
+      user: { user },
+    });
+  } catch (err) {
+    next(new HttpError("Something went wrong", 500));
+  }
+};
--- server/routes/users-route.js
+++ server/routes/users-route.js
@@ -2,11 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.json({
-    status: "success",
-    message: " users middleware runs",
-  });
-});
+const {
+  getAllUsers,
+  createUser,
+  loginUser,
+} = require("./../controllers/user-controller");
+
+router.get("/", getAllUsers);
+
+// router.get("/:uId", getUserById);
+
+router.post("/signup", createUser);
+
+router.post("/login", loginUser);
+
+// router.patch("/:uId", updateUser);
+
+// router.delete("/:uId", deleteUser);
 
 module.exports = router;
