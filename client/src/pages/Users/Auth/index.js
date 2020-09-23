efb5bd8f7685d56ab178404e0ac8286815a39121
marwa <marwajomaa4@gmail.com>
2020-09-23 11:59:32 +0300

    send signup request and handling error

--- client/src/pages/Users/Auth/index.js
+++ client/src/pages/Users/Auth/index.js
@@ -5,6 +5,8 @@ import Input from "../../../common/Input";
 import Button from "../../../common/Button";
 import Card from "../../../common/Card";
 import Title from "../../../common/Title";
+import LoadingSpinner from "../../../common/LoadingSpinner";
+import ErrorModal from "../../../common/ErrorModal";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_EMAIL,
@@ -19,6 +21,8 @@ const Auth = () => {
   const { login } = auth;
 
   const [isSignup, setIsSignup] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setError] = useState();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -59,6 +63,7 @@ const Auth = () => {
   };
 
   const authSubmitHandler = async (e) => {
+    setIsLoading(true);
     e.preventDefault();
     if (isSignup) {
       try {
@@ -68,70 +73,89 @@ const Auth = () => {
           password: formState.inputs.password.value,
         };
 
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-
-        // Request body
-
         const response = await axios.post("api/users/signup", data);
-        console.log(response.data, "--------------");
+        console.log(response);
+        if (!response.status === 200) {
+          throw new Error(
+            response.message || "Something wrong in fetching data"
+          );
+        }
+        setIsLoading(false);
       } catch (err) {
-        console.log(err);
+        console.log(err.response.data.error);
+        setError(
+          err.response.data.error || "Something went wrong, please try again"
+        );
+        setIsLoading(false);
       }
-      login();
+      // login();
     } else {
       console.log("hi");
     }
+  };
+
+  const handleError = () => {
+    setError();
   };
 
   return (
     <>
       <Title label="Login to your account" />
       <Card className="authentication">
-        <h2 className="authentication__header">{`${
-          isSignup ? "Signup for new account" : "Login Required"
-        }`}</h2>
-        <hr />
-        <form className="center" onSubmit={authSubmitHandler}>
-          {isSignup && (
-            <Input
-              type="text"
-              element="input"
-              id="name"
-              validators={[VALIDATOR_REQUIRE()]}
-              label="Enter your name"
-              errorText="Please enter a name."
-              onInput={inputHandler}
-            />
-          )}
-          <Input
-            type="email"
-            element="input"
-            id="email"
-            label="Enter your email"
-            errorText="Please enter a valid email."
-            validators={[VALIDATOR_EMAIL()]}
-            onInput={inputHandler}
-          />
-          <Input
-            type="password"
-            element="input"
-            id="password"
-            label="Enter your password"
-            errorText="Please enter a valid password (at least 6 characters)."
-            validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6)]}
-            onInput={inputHandler}
-          />
-          <Button type="submit" disabled={!formState.isFormValid}>
-            {isSignup ? "SIGNUP" : "LOGIN"}
-          </Button>
-        </form>
-        <Button inverse onClick={switchAuthHandler}>
-          Switch to {`${isSignup ? "login" : "signup"}`}
-        </Button>
+        {isLoading ? (
+          <LoadingSpinner asOverlay />
+        ) : isError ? (
+          <ErrorModal error={isError} onClear={handleError} />
+        ) : (
+          <>
+            <h2 className="authentication__header">{`${
+              isSignup ? "Signup for new account" : "Login Required"
+            }`}</h2>
+            <hr />
+            <form className="center" onSubmit={authSubmitHandler}>
+              {isSignup && (
+                <Input
+                  type="text"
+                  element="input"
+                  id="name"
+                  validators={[VALIDATOR_REQUIRE()]}
+                  label="Enter your name"
+                  errorText="Please enter a name."
+                  onInput={inputHandler}
+                />
+              )}
+              <Input
+                type="email"
+                element="input"
+                id="email"
+                label="Enter your email"
+                errorText="Please enter a valid email."
+                validators={[VALIDATOR_EMAIL()]}
+                onInput={inputHandler}
+              />
+              <Input
+                type="password"
+                element="input"
+                id="password"
+                label="Enter your password"
+                errorText="Please enter a valid password (at least 6 characters)."
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6)]}
+                onInput={inputHandler}
+              />
+              <Button type="submit" disabled={!formState.isFormValid}>
+                {isSignup ? "SIGNUP" : "LOGIN"}
+              </Button>
+            </form>
+            <p>
+              {isSignup
+                ? "Already have an account?"
+                : "Don't have account yet?"}
+            </p>
+            <Button inverse onClick={switchAuthHandler}>
+              Switch to {`${isSignup ? "login" : "signup"}`}
+            </Button>
+          </>
+        )}
       </Card>
     </>
   );
